163f649ad403e4f582c7a68b7b8c42b6f2c1cb04
faustbrian <faustbrian@users.noreply.github.com>
2023-04-26 03:36:44 +0000

    chore: resolve style guide violations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,6 +12,6 @@ final class ServiceProvider extends AbstractServiceProvider
 {
     public function packageRegistered(): void
     {
-        IconFamilyRegistry::push(IconFamily::fromDirectory('ionicons', __DIR__ . '/../resources/svg'));
+        IconFamilyRegistry::push(IconFamily::fromDirectory('ionicons', __DIR__.'/../resources/svg'));
     }
 }
